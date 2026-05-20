a19c5fe061cf74a2df1f18b8354e8f18e3a650b8
Rikentod <annadiumheart@gmail.com>
2026-05-21 01:34:02 +0200

    All Races Done

--- src/main/java/com/annadium/createtensura/configs/ConstructConfig.java
+++ src/main/java/com/annadium/createtensura/configs/ConstructConfig.java
@@ -10,6 +10,10 @@ import lombok.Generated;
 @SyncToClient
 public class ConstructConfig extends ManasConfig {
     public Construct construct = new Construct();
+    public Robot robot = new Robot();
+    public Mecha mecha = new Mecha();
+    public Gundam gundam = new Gundam();
+    public Warframe warframe = new Warframe();
 
     public ConstructConfig() {
 
@@ -47,6 +51,398 @@ public class ConstructConfig extends ManasConfig {
         public double swimSpeed = -0;
         public Construct() {
 
+        }
+
+        @Generated
+        public double getMinAura() {
+            return this.minAura;
+        }
+
+        @Generated
+        public double getMaxAura() {
+            return this.maxAura;
+        }
+
+        @Generated
+        public double getMinMagicule() {
+            return this.minMagicule;
+        }
+
+        @Generated
+        public double getMaxMagicule() {
+            return this.maxMagicule;
+        }
+
+        @Generated
+        public double getSize() {
+            return this.size;
+        }
+
+        @Generated
+        public double getMaxHealth() {
+            return this.maxHealth;
+        }
+
+        @Generated
+        public double getMaxSpiritualHealth() {
+            return this.maxSpiritualHealth;
+        }
+
+        @Generated
+        public double getAttack() {
+            return this.attack;
+        }
+
+        @Generated
+        public double getAttackSpeed() {
+            return this.attackSpeed;
+        }
+
+        @Generated
+        public double getKnockbackResistance() {
+            return this.knockbackResistance;
+        }
+
+        @Generated
+        public double getMovementSpeed() {
+            return this.movementSpeed;
+        }
+
+        @Generated
+        public double getSwimSpeed() {
+            return this.swimSpeed;
+        }
+    }
+
+    public static class Robot extends RaceConfig.Default {
+        @Comment("EP requirement to evolve into a Robot.")
+        public double epRequirement = 20000;
+        @Comment("Minimal aura.")
+        public double minAura = 100;
+        @Comment("Maximum aura.")
+        public double maxAura = 1000;
+        @Comment("Minimal magicule.")
+        public double minMagicule = 4500;
+        @Comment("Maximum magicule.")
+        public double maxMagicule = 9000;
+        @Comment("Bonus Size.")
+        public double size = 0;
+        @Comment("Bonus Max Health.")
+        public double maxHealth = 10;
+        @Comment("Bonus Max Spiritual Health.")
+        public double maxSpiritualHealth = 40.0F;
+        @Comment("Bonus Attack Damage.")
+        public double attack = 1;
+        @Comment("Bonus Attack Speed.")
+        public double attackSpeed = 0;
+        @Comment("Bonus Knockback Resistance.")
+        public double knockbackResistance = 0.65F;
+        @Comment("Bonus Movement Speed.")
+        public double movementSpeed = -0;
+        @Comment("Bonus Swimming Speed Multiplier.")
+        public double swimSpeed = -0;
+
+        public Robot() {
+
+        }
+
+        @Generated
+        public double getEpRequirement() {
+            return this.epRequirement;
+        }
+
+        @Generated
+        public double getMinAura() {
+            return this.minAura;
+        }
+
+        @Generated
+        public double getMaxAura() {
+            return this.maxAura;
+        }
+
+        @Generated
+        public double getMinMagicule() {
+            return this.minMagicule;
+        }
+
+        @Generated
+        public double getMaxMagicule() {
+            return this.maxMagicule;
+        }
+
+        @Generated
+        public double getSize() {
+            return this.size;
+        }
+
+        @Generated
+        public double getMaxHealth() {
+            return this.maxHealth;
+        }
+
+        @Generated
+        public double getMaxSpiritualHealth() {
+            return this.maxSpiritualHealth;
+        }
+
+        @Generated
+        public double getAttack() {
+            return this.attack;
+        }
+
+        @Generated
+        public double getAttackSpeed() {
+            return this.attackSpeed;
+        }
+
+        @Generated
+        public double getKnockbackResistance() {
+            return this.knockbackResistance;
+        }
+
+        @Generated
+        public double getMovementSpeed() {
+            return this.movementSpeed;
+        }
+
+        @Generated
+        public double getSwimSpeed() {
+            return this.swimSpeed;
+        }
+    }
+
+    public static class Mecha extends RaceConfig.Default {
+        @Comment("EP requirement to evolve into a Mecha.")
+        public double epRequirement = 150000;
+        @Comment("Minimal aura.")
+        public double minAura = 50000;
+        @Comment("Maximum aura.")
+        public double maxAura = 50000;
+        @Comment("Minimal magicule.")
+        public double minMagicule = 14500;
+        @Comment("Maximum magicule.")
+        public double maxMagicule = 19000;
+        @Comment("Bonus Size.")
+        public double size = 1.5;
+        @Comment("Bonus Max Health.")
+        public double maxHealth = 90;
+        @Comment("Bonus Max Spiritual Health.")
+        public double maxSpiritualHealth = 300.0F;
+        @Comment("Bonus Attack Damage.")
+        public double attack = 2;
+        @Comment("Bonus Attack Speed.")
+        public double attackSpeed = 0;
+        @Comment("Bonus Knockback Resistance.")
+        public double knockbackResistance = 0.8F;
+        @Comment("Bonus Movement Speed.")
+        public double movementSpeed = -0;
+        @Comment("Bonus Swimming Speed Multiplier.")
+        public double swimSpeed = -0;
+
+        public Mecha() {
+
+        }
+
+        @Generated
+        public double getEpRequirement() {
+            return this.epRequirement;
+        }
+
+        @Generated
+        public double getMinAura() {
+            return this.minAura;
+        }
+
+        @Generated
+        public double getMaxAura() {
+            return this.maxAura;
+        }
+
+        @Generated
+        public double getMinMagicule() {
+            return this.minMagicule;
+        }
+
+        @Generated
+        public double getMaxMagicule() {
+            return this.maxMagicule;
+        }
+
+        @Generated
+        public double getSize() {
+            return this.size;
+        }
+
+        @Generated
+        public double getMaxHealth() {
+            return this.maxHealth;
+        }
+
+        @Generated
+        public double getMaxSpiritualHealth() {
+            return this.maxSpiritualHealth;
+        }
+
+        @Generated
+        public double getAttack() {
+            return this.attack;
+        }
+
+        @Generated
+        public double getAttackSpeed() {
+            return this.attackSpeed;
+        }
+
+        @Generated
+        public double getKnockbackResistance() {
+            return this.knockbackResistance;
+        }
+
+        @Generated
+        public double getMovementSpeed() {
+            return this.movementSpeed;
+        }
+
+        @Generated
+        public double getSwimSpeed() {
+            return this.swimSpeed;
+        }
+    }
+
+    public static class Gundam extends RaceConfig.Default {
+        @Comment("EP requirement to evolve into a Gundam.")
+        public double epRequirement = 400000;
+        @Comment("Minimal aura.")
+        public double minAura = 200000;
+        @Comment("Maximum aura.")
+        public double maxAura = 200000;
+        @Comment("Minimal magicule.")
+        public double minMagicule = 900000;
+        @Comment("Maximum magicule.")
+        public double maxMagicule = 900000;
+        @Comment("Bonus Size.")
+        public double size = 4;
+        @Comment("Bonus Max Health.")
+        public double maxHealth = 490;
+        @Comment("Bonus Max Spiritual Health.")
+        public double maxSpiritualHealth = 2990.0F;
+        @Comment("Bonus Attack Damage.")
+        public double attack = 3;
+        @Comment("Bonus Attack Speed.")
+        public double attackSpeed = 0;
+        @Comment("Bonus Knockback Resistance.")
+        public double knockbackResistance = 0.95F;
+        @Comment("Bonus Movement Speed.")
+        public double movementSpeed = -0;
+        @Comment("Bonus Swimming Speed Multiplier.")
+        public double swimSpeed = -0;
+
+        public Gundam() {
+
+        }
+
+        @Generated
+        public double getEpRequirement() {
+            return this.epRequirement;
+        }
+
+        @Generated
+        public double getMinAura() {
+            return this.minAura;
+        }
+
+        @Generated
+        public double getMaxAura() {
+            return this.maxAura;
+        }
+
+        @Generated
+        public double getMinMagicule() {
+            return this.minMagicule;
+        }
+
+        @Generated
+        public double getMaxMagicule() {
+            return this.maxMagicule;
+        }
+
+        @Generated
+        public double getSize() {
+            return this.size;
+        }
+
+        @Generated
+        public double getMaxHealth() {
+            return this.maxHealth;
+        }
+
+        @Generated
+        public double getMaxSpiritualHealth() {
+            return this.maxSpiritualHealth;
+        }
+
+        @Generated
+        public double getAttack() {
+            return this.attack;
+        }
+
+        @Generated
+        public double getAttackSpeed() {
+            return this.attackSpeed;
+        }
+
+        @Generated
+        public double getKnockbackResistance() {
+            return this.knockbackResistance;
+        }
+
+        @Generated
+        public double getMovementSpeed() {
+            return this.movementSpeed;
+        }
+
+        @Generated
+        public double getSwimSpeed() {
+            return this.swimSpeed;
+        }
+    }
+
+    public static class Warframe extends RaceConfig.Default {
+        @Comment("EP requirement to evolve into a Warframe.")
+        public double epRequirement = 400000;
+        @Comment("Minimal aura.")
+        public double minAura = 200000;
+        @Comment("Maximum aura.")
+        public double maxAura = 200000;
+        @Comment("Minimal magicule.")
+        public double minMagicule = 900000;
+        @Comment("Maximum magicule.")
+        public double maxMagicule = 900000;
+        @Comment("Bonus Size.")
+        public double size = 0;
+        @Comment("Bonus Max Health.")
+        public double maxHealth = 390;
+        @Comment("Bonus Max Spiritual Health.")
+        public double maxSpiritualHealth = 2990.0F;
+        @Comment("Bonus Attack Damage.")
+        public double attack = 3;
+        @Comment("Bonus Attack Speed.")
+        public double attackSpeed = 0.1;
+        @Comment("Bonus Knockback Resistance.")
+        public double knockbackResistance = 0.95F;
+        @Comment("Bonus Movement Speed.")
+        public double movementSpeed = 0.1;
+        @Comment("Bonus Swimming Speed Multiplier.")
+        public double swimSpeed = 0.1;
+
+        public Warframe() {
+
+        }
+
+        @Generated
+        public double getEpRequirement() {
+            return this.epRequirement;
         }
 
         @Generated
--- src/main/java/com/annadium/createtensura/races/WarframeRace.java
+++ src/main/java/com/annadium/createtensura/races/WarframeRace.java
@@ -2,61 +2,51 @@ package com.annadium.createtensura.races;
 
 import com.annadium.createtensura.configs.ConstructConfig;
 import com.annadium.createtensura.registry.RaceRegistry;
-import com.annadium.createtensura.registry.SkillRegistry;
 import io.github.manasmods.manascore.config.ConfigRegistry;
 import io.github.manasmods.manascore.race.api.ManasRace;
 import io.github.manasmods.manascore.race.api.ManasRaceInstance;
-import io.github.manasmods.manascore.skill.api.ManasSkill;
 import io.github.manasmods.tensura.config.race.RaceConfig;
-import io.github.manasmods.tensura.race.template.DefaultRace;
-import io.github.manasmods.tensura.storage.Alignment;
+import io.github.manasmods.tensura.race.template.EvolutionRequirement;
 import net.minecraft.world.entity.LivingEntity;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
-public class ConstructRace extends DefaultRace {
-    public ConstructRace() {
-        super(Difficulty.EASY);
-        this.applyDefaultAttributeModifiers();
-    }
+public class WarframeRace extends MechaRace {
 
     @Override
     public RaceConfig.Default getDefaultConfig() {
-        return ConfigRegistry.getConfig(ConstructConfig.class).construct;
+        return ConfigRegistry.getConfig(ConstructConfig.class).warframe;
+    }
+
+    @Override
+    public Map<EvolutionRequirement, Float> getEvolutionRequirements(ManasRaceInstance previous, LivingEntity entity) {
+        return Map.of(new EvolutionRequirement.EPRequirement(ConfigRegistry.getConfig(ConstructConfig.class).warframe.getEpRequirement()), 100f);
     }
 
     @Override
     public @Nullable ManasRace getDefaultEvolution(ManasRaceInstance instance, LivingEntity entity) {
-        return RaceRegistry.ROBOT.get();
+        return null;
     }
 
     @Override
     public @Nullable ManasRace getAwakeningEvolution(ManasRaceInstance instance, LivingEntity entity) {
-        return RaceRegistry.ROBOT.get();
+        return null;
     }
 
     @Override
     public @Nullable ManasRace getHarvestFestivalEvolution(ManasRaceInstance instance, LivingEntity entity) {
-        return RaceRegistry.ROBOT.get();
+        return null;
     }
 
     @Override
     public List<ManasRace> getNextEvolutions(ManasRaceInstance instance, LivingEntity entity) {
-        return List.of(RaceRegistry.ROBOT.get());
+        return List.of();
     }
 
     @Override
-    public Alignment getAlignment() {
-        return Alignment.MAJIN;
-    }
-
-    @Override
-    public List<ManasSkill> getIntrinsicSkills(ManasRaceInstance instance, LivingEntity entity) {
-        List<ManasSkill> list = new ArrayList<>();
-        list.add(SkillRegistry.MECHANICAL_EYE_SKILL.get());
-        list.add(SkillRegistry.MECHANICAL_HAND.get());
-        return list;
+    public List<ManasRace> getPreviousEvolutions(ManasRaceInstance instance, LivingEntity entity) {
+        return List.of(RaceRegistry.MECHA.get());
     }
 }
